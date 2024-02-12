8f1c9ee832046f8178a449eff9a4a12ac1ffdac3
Giorgi Gvekhidze <92603830+Gvekho@users.noreply.github.com>
2024-02-12 11:32:35 +0400

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def data_manipulation(df):
     df.drop('Identifying information', inplace=True, axis=1)
 
     df[['Name', 'AKA']] = df['Name'].str.split('\n', expand=True)
-    df['AKA'] = df['AKA'].astype(str).str.replace('(a.k.a. ', '').str.replace(')', '')
+    df['AKA'] = df['AKA'].astype(str).str.replace('(a.k.a. ', '').str.replace('(a.k.a.', '').str.replace(')', '').str.replace('/', '')
 
     aka = df[['AKA', 'Date']][df['AKA'] != 'None']
     aka.columns = ['Name', 'Date']
